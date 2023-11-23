33c9cface7225a6482855c02384234dee933b110
Uzma123 <uzmafarheen.hi@tcs.com>
2023-11-23 10:32:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const[lightUp,setLightUp]=useState([]);
+  const element=[[0,1,2],[3,4,5],[6,7,8]]
+const handleClick=(idx)=>{
+    let array=[...lightUp,idx];
+    setLightUp(array);
+}
+
+useEffect(()=>{
+ if(lightUp.length===9)
+  {
+    // let i=0;
+   let timeInterval= setInterval(()=>{
+      lightUp.splice(0,1);// first se last se just pop and with splice se we can do
+      setLightUp([...lightUp]);// pop is very importantly important jab kuch bhi nahi hpga 
+    },200)
+  }
+
+},[lightUp])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 style={{fontSize:'3rem' }} >This is My Tic Toc Teo App </h1>
+    <div className="tic-toc-container">
+     { element.map((ele,ind)=>{
+       return <div className="first-container" key={ind}>
+       { ele.map((box,index)=>{
+        // if(box==1)
+        return <div key={box} className={lightUp.includes(box)===true?"box boxBackground":"box"} onClick={()=>{handleClick(box)}}></div>
+       })
+       }
+     </div>
+      })}
+     </div>
     </div>
   );
 }
